ec61b7b6e5ea145438cf9f633687e55e2c6fd261
SENECAL NICOLAS <nsenecal@localhost.localdomain>
2018-12-14 17:38:01 +0100

    update Error model

--- Error/include/Error/Error.hpp
+++ Error/include/Error/Error.hpp
@@ -1,60 +1,63 @@
 /**
  * \file Error.hpp
- * \brief 
+ * \brief Declaration of the class "Error"
  */
-
-#include <exception>
-#include <string>
 
 #ifndef __ERROR_HPP__
 #define __ERROR_HPP__
 
-#define AT __FILE__, __LINE__, __func__
+#include <exception>
+#include <string>
 
 /**
- * \class Erreur
- * \brief Classe de gestion des erreurs, hérité de std::exception
+ * \class Error
+ * \brief Error managment, inherited from std::exception
  */
 class Error : public std::exception {
 protected:
-  const std::string _message;
-  const char* _file;
-  const unsigned int _line;
-  const char* _function;
+  const std::string _message; /*!< the error message */
+  const std::string _file; /*!< file where had the error */
+  const unsigned int _line; /*!< line where had the error */
+  const std::string _function; /*!< function where had the error */
 
 public:
 
   /**
-   * \brief Constructeur par défaut
+   * \brief Default constructor
    */
   Error() : _message(""), _file(""), _line(0), _function("") {
   };
 
   /**
-   * \brief Constructeur
-   * \param message : message d'erreur a afficher
-   * \param file : fichier de l'erreur
-   * \param line : ligne de l'erreur
-   * \param function : fonction où a eu lieu l'erreur
+   * \brief Constructor for development errors
+   * \param message : error message
+   * \param file : file where had the error 
+   * \param line : line where had the error
+   * \param function : function where had the error
    */
-  Error(const std::string &message, const char* file, const unsigned int line, const char* function) : _message(message), _file(file), _line(line), _function(function) {
+  Error(const std::string &message, const std::string &file, const unsigned int line, const std::string function) noexcept : _message(message), _file(file), _line(line), _function(function) {
   }
 
   /**
-   * \brief Destructeur
+   * \brief destructor
    */
   ~Error() {
   }
 
   /**
-   * \brief Obtenir toutes les informations de l'erreur pour l'afficher
-   * \return Une chaine avec toutes les informations de l'erreur
+   * \brief Get all the information from the error to display it
+   * \return A string with all the information of the error
    */
   virtual
   const char* what() const noexcept {
-    std::string *err = new std::string("Error : " + std::string(_file) + ":" + std::to_string(_line) + "::" + std::string(_function) + "\n  Message: " + _message);
+    std::string *err = new std::string("Error : " + _file + ":" + std::to_string(_line) + "::" + _function + "\n  Message: " + _message);
     return err->c_str();
   }
+
+  /** 
+  * The error macro
+  */
+  #define ERROR_AT __FILE__, __LINE__, __func__
 };
 
 
