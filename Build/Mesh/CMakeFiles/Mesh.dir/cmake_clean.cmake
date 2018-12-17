0d2a61e08e2aa0ef5753f561db8beea448caa4c5
afadili <aminafadili@gmail.com>
2018-12-17 10:49:32 +0100

    git fix

---
+++ Build/Mesh/CMakeFiles/Mesh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mesh.dir/include/Mesh/Sphere.cpp.o"
+  "libMesh.pdb"
+  "libMesh.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mesh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
