055ac91e73f11b7dd6ee9c69cea7b3bd2519dadf
SENECAL NICOLAS <nsenecal@localhost.localdomain>
2018-12-17 18:43:39 +0100

    Added reading json file (to finish)

--- Game/src/GameManager.cpp
+++ Game/src/GameManager.cpp
@@ -5,10 +5,26 @@ GameManager::GameManager(const FilePath &assetPath){
 }
 
 void GameManager::loadAssets(const FilePath &assetPath){
-	/*std::ifstream assetJSON(assetPath);
-    Json::Reader reader;
-    Json::Value obj;
-    reader.parse(assetJSON, obj);     // Reader can also read strings
-    std::cout << "Last name: " << obj["lastname"].asString() << std::endl;
-    std::cout << "First name: " << obj["firstname"].asString() << std::endl;*/
+	std::ifstream assetJSON(assetPath, std::ifstream::binary);
+	if(assetJSON.fail())
+		std::cout << "FILE DON'T EXIST" << std::endl;
+
+    Json::Value root;   // will contain the root value after parsing.
+    Json::CharReaderBuilder builder;
+    std::string errs;
+
+    if (!Json::parseFromStream(builder, assetJSON, &root, &errs)) {
+        // report to the user the failure and their locations in the document.
+        std::cout  << errs << std::endl;
+    }
+     
+
+    const Json::Value& shaders = root["shaders"]; // array of characters
+    for (int i = 0; i < shaders.size(); i++){
+        std::cout << "    name: " << shaders[i]["name"].asString();
+        std::cout << "      fs: " << shaders[i]["fs"].asString();
+        std::cout << "      vs: " << shaders[i]["vs"].asString();
+        std::cout << std::endl;
+    }
+
 }
