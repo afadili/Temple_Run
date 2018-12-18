b67c2e5e4d7be6b5b4b5cd85a10180ef3412a06b
afadili <aminafadili@gmail.com>
2018-12-18 09:27:19 +0100

    window

--- TEMPLE_RUN/main.cpp
+++ TEMPLE_RUN/main.cpp
@@ -18,46 +18,50 @@ using namespace glimac;
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
 
 int main(int argc, char **argv) {
-  // Initialize SDL and open a window
-    SDLWindowManager windowManager(800, 600, "I N F I N I T Y  R U N");
+	// TEST GAME MANAGER
+	GameManager manager("TEMPLE_RUN/assets.json");
 
-    // Initialize glew for OpenGL3+ support
-    GLenum glewInitError = glewInit();
-    if (GLEW_OK != glewInitError) {
-        std::cerr << glewGetErrorString(glewInitError) << std::endl;
-        return EXIT_FAILURE;
-    }
 
-    std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl;
+	// Initialize SDL and open a window
+	SDLWindowManager windowManager(800, 600, "I N F I N I T Y  R U N");
 
-    /*********************************
-     * HERE SHOULD COME THE INITIALIZATION CODE
-     *********************************/
+	// Initialize glew for OpenGL3+ support
+	GLenum glewInitError = glewInit();
+	if (GLEW_OK != glewInitError) {
+	    std::cerr << glewGetErrorString(glewInitError) << std::endl;
+	    return EXIT_FAILURE;
+	}
 
-    // Application loop:
-    bool done = false;
-    while (!done) {
-        Uint32 startTime = SDL_GetTicks();
-        // Event loop:
-        SDL_Event e;
-        while (windowManager.pollEvent(e)) {
-            if (e.type == SDL_QUIT) {
-                done = true; // Leave the loop after this iteration
-            }
-        }
+	std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
+	std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl;
 
-        /*********************************
-         * HERE SHOULD COME THE RENDERING CODE
-         *********************************/
+	/*********************************
+	 * HERE SHOULD COME THE INITIALIZATION CODE
+	 *********************************/
 
-        // Update the display
-        windowManager.swapBuffers();
-        Uint32 elapsedTime = SDL_GetTicks() - startTime;
-        if (elapsedTime < FRAMERATE_MILLISECONDS) {
-            SDL_Delay(FRAMERATE_MILLISECONDS - elapsedTime);
-        }
-    }
+	// Application loop:
+	bool done = false;
+	while (!done) {
+	    Uint32 startTime = SDL_GetTicks();
+	    // Event loop:
+	    SDL_Event e;
+	    while (windowManager.pollEvent(e)) {
+	        if (e.type == SDL_QUIT) {
+	            done = true; // Leave the loop after this iteration
+	        }
+	    }
+
+	    /*********************************
+	     * HERE SHOULD COME THE RENDERING CODE
+	     *********************************/
+
+	    // Update the display
+	    windowManager.swapBuffers();
+	    Uint32 elapsedTime = SDL_GetTicks() - startTime;
+	    if (elapsedTime < FRAMERATE_MILLISECONDS) {
+	        SDL_Delay(FRAMERATE_MILLISECONDS - elapsedTime);
+	    }
+	}
 
     return EXIT_SUCCESS;
 }
