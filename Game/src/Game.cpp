0ffdb15e4b1e1a96f1de754e85d584ea08faaf92
ROUSSET CECILE <crouss20@localhost.localdomain>
2018-12-18 15:51:25 +0100

    Added colors to ppm reader

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -3,67 +3,104 @@
 
 using namespace glimac;
 
-Game::Game(){
-
+Game::Game(const FilePath &path, int nbFloor){
+	loadMap(path, nbFloor);
 }
 
 void Game::loadMap(const FilePath &path, int nbFloor){
 
 	/*for(int i = 0; i<nbFloor; i++){
-		loadFloor(path + std::to_string(i) + ".txt");
+		loadFloor(path + std::to_string(i) + ".ppm");
 	}*/
 }
 
-void loadFloor(const FilePath &file){
-	std::ifstream fileLevel(file);  //Ouverture d'un fichier en lecture
+void Game::loadFloor(const FilePath &file, int floor){
+	std::ifstream fileLevel(file);
 
-	int w, h, i, j, r, g, b, max;
-	
-	//If we can read the file
-	if(fileLevel){
+	if(fileLevel.fail())
+		throw Error("The ppm file \"" + file.str() +"\" is not found !", "FILE_NOT_FOUND", true);
 
-		//First line with p3
-		std::string line;
-		getline(fileLevel, line);
+	if(file.ext() != "ppm")
+		throw Error("The file \"" + file.str() +"\" as to be a PPM file !", "INCORRECT_FILE", true);
 
-		//Second line with width and height
-		fileLevel >> w;
-		fileLevel >> h;
+    int w, h, i, j, r, g, b, max, level;
 
-		std::cout << "w : " << w << std::endl;
-		std::cout << "h : " << h << std::endl;
+    //First line with p3
+	std::string line;
+	getline(fileLevel, line);
 
-		//Third line with maximal color value
-		fileLevel >> max;
+	//Second line with width, height and floor of the map
+	fileLevel >> w;
+	fileLevel >> h;
 
-		for(i=0 ; i<h; i++){
-			for(j=0 ; j<w; j++){
-				fileLevel >> r;
-				fileLevel >> g;
-				fileLevel >> b;
+	//Third line with maximal color value
+	fileLevel >> max;
 
-				if(r==max && g==0 && b==0){
-					std::cout << "Red" << std::endl;					
-					std::cout << "Coordonnées : " << i << "," << j << std::endl;					
-				}
+	for(i=0 ; i<h; i++){
+		for(j=0 ; j<w; j++){
+			fileLevel >> r;
+			fileLevel >> g;
+			fileLevel >> b;
 
-				if(r==0 && g==max && b==0){
-					std::cout << "Green" << std::endl;					
-				}
+			if(r==143 && g==94 && b==10){
+				//std::cout << "Brown" << std::endl;
+				//std::cout << "Ground" << std::endl;					
+			}
 
-				if(r==0 && g==0 && b==max){
-					std::cout << "Blue" << std::endl;					
-				}
+			if(r==143 && g==136 && b==131){
+				//std::cout << "Grey" << std::endl;
+				//std::cout << "Wall" << std::endl;					
+			}
 
-				if(r==143 && g==94 && b==10){ //Brown : ground
-					std::cout << "Brown" << std::endl;					
-				}
+			if(r==152 && g==21 && b==49){
+				//std::cout << "Burgundy" << std::endl;
+				//std::cout << "Hole" << std::endl;					
+			}
+
+			if(r==255 && g==207 && b==171){
+				//std::cout << "Beige" << std::endl;
+				//std::cout << "Rock" << std::endl;					
+			}
+
+			if(r==0 && g==0 && b==0){
+				//std::cout << "Black" << std::endl;
+				//std::cout << "Camera" << std::endl;					
+			}
+
+			if(r==39 && g==203 && b==205){
+				//std::cout << "Turquoise" << std::endl;
+				//std::cout << "Character" << std::endl;					
+			}
+
+			if(r==max && g==0 && b==0){
+				//std::cout << "Red" << std::endl;					
+				//std::cout << "Reality Stone" << std::endl;			
+			}
+
+			if(r==0 && g==max && b==0){
+				//std::cout << "Green" << std::endl;
+				//std::cout << "Time Stone" << std::endl;				
+			}
+
+			if(r==0 && g==0 && b==max){
+				//std::cout << "Blue" << std::endl;
+				//std::cout << "Space Stone" << std::endl;				
+			}
+
+			if(r==max && g==max && b==0){
+				//std::cout << "Yellow" << std::endl;
+				//std::cout << "Mind Stone" << std::endl;					
+			}
+
+			if(r==max && g==0 && b==max){
+				//std::cout << "Purple" << std::endl;
+				//std::cout << "Power Stone" << std::endl;					
+			}
+
+			if(r==max && g==162 && b==0){
+				//std::cout << "Orange" << std::endl;
+				//std::cout << "Soul Stone" << std::endl;					
 			}
 		}
-
-	}
-	//If we can't read it
-	else{
-	    std::cout << "ERREUR: Impossible d'ouvrir le fichier." << std::endl;
 	}
 }
