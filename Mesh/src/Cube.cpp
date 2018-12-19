0baa5bad30ccafd1f42e6bac58bdcd017374b988
SENECAL NICOLAS <nsenecal@localhost.localdomain>
2018-12-19 16:00:05 +0100

    Functional cube (normal has to be reviewed)

--- Mesh/src/Cube.cpp
+++ Mesh/src/Cube.cpp
@@ -2,12 +2,12 @@
 #include <glimac/common.hpp>
 #include <Mesh/Cube.hpp>
 
-Cube::Cube() : Mesh() {
+Cube::Cube() : Mesh(nullptr, nullptr){
 	generateVertices(glm::vec3(1.,1.,1.));
 	generateIndexs();
 }
 
-Cube::Cube(const glm::vec3 &width) : Mesh() {
+Cube::Cube(const glm::vec3 &width) : Mesh(nullptr, nullptr) {
 	generateVertices(width);
 	generateIndexs();
 }
@@ -24,28 +24,31 @@ Cube::Cube(const glm::vec3 &width, ShaderManager *shader, Texture *texture) : Me
 
 
 void Cube::generateVertices(const glm::vec3 &width){
-	// Center the mesh 
-	glm::vec3 upper = glm::vec3(width.x/2., width.y/2., width.z/2.);
-	glm::vec3 lower = glm::vec3(-width.x/2., -width.y/2., -width.z/2.);
+    float w,h,t;
+    w = width.x/2;        
+    h = width.y/2;   
+	t = width.z/2; 
 
 	// FRONT FACE
-    m_vertices.push_back(ShapeVertex(glm::vec3(upper.x, upper.y, upper.z), glm::vec3(upper.x, upper.y, upper.z), glm::vec2(1,1))); // TOP RIGHT
-    m_vertices.push_back(ShapeVertex(glm::vec3(upper.x, lower.y, upper.z), glm::vec3(upper.x, lower.y, upper.z), glm::vec2(1,0))); // BOTTOM RIGHT
-    m_vertices.push_back(ShapeVertex(glm::vec3(lower.x, lower.y, upper.z), glm::vec3(lower.x, lower.y, upper.z), glm::vec2(0,0))); // BOTTOM LEFT
-    m_vertices.push_back(ShapeVertex(glm::vec3(lower.x, upper.y, upper.z), glm::vec3(lower.x, upper.y, upper.z), glm::vec2(0,1))); // TOP LEFT
+	//										POSITION			NORMAL				TEXTURE
+    m_vertices.push_back(ShapeVertex(glm::vec3(-w, -h, t), glm::vec3(0, 0, 1), glm::vec2(1,1))); // BOTTOM LEFT
+    m_vertices.push_back(ShapeVertex(glm::vec3(w, -h, t), glm::vec3(0, 0, 1), glm::vec2(1,0))); // BOTTOM RIGHT
+    m_vertices.push_back(ShapeVertex(glm::vec3(w, h, t), glm::vec3(0, 0, 1), glm::vec2(0,0))); // TOP RIGHT
+    m_vertices.push_back(ShapeVertex(glm::vec3(-w, h, t), glm::vec3(0, 0, 1), glm::vec2(0,1))); // TOP LEFT
 
     // BACK FACE
-    m_vertices.push_back(ShapeVertex(glm::vec3(upper.x, upper.y, lower.z), glm::vec3(upper.x, upper.y, lower.z), glm::vec2(1,1))); // TOP RIGHT
-    m_vertices.push_back(ShapeVertex(glm::vec3(upper.x, lower.y, lower.z), glm::vec3(upper.x, lower.y, lower.z), glm::vec2(1,0))); // BOTTOM RIGHT
-    m_vertices.push_back(ShapeVertex(glm::vec3(lower.x, lower.y, lower.z), glm::vec3(lower.x, lower.y, lower.z), glm::vec2(0,0))); // BOTTOM LEFT
-    m_vertices.push_back(ShapeVertex(glm::vec3(lower.x, upper.y, lower.z), glm::vec3(lower.x, upper.y, lower.z), glm::vec2(0,1))); // TOP LEFT
+	//										POSITION			NORMAL				TEXTURE
+    m_vertices.push_back(ShapeVertex(glm::vec3(-w, -h, -t), glm::vec3(0, 0, -1), glm::vec2(1,1))); // TOP RIGHT
+    m_vertices.push_back(ShapeVertex(glm::vec3(w, -h, -t), glm::vec3(0, 0, -1), glm::vec2(1,0))); // BOTTOM RIGHT
+    m_vertices.push_back(ShapeVertex(glm::vec3(w, h, -t), glm::vec3(0, 0, -1), glm::vec2(0,0))); // BOTTOM LEFT
+    m_vertices.push_back(ShapeVertex(glm::vec3(-w, h, -t), glm::vec3(0, 0, -1), glm::vec2(0,1))); // TOP LEFT
 }
 
 void Cube::generateIndexs(){
-	m_indexVer.insert(m_indexVer.end(), { 0, 1, 2, 0, 2, 3 }); // FRONT FACE
-	m_indexVer.insert(m_indexVer.end(), { 4, 5, 6, 4, 6, 7 }); // BACK FACE
-	m_indexVer.insert(m_indexVer.end(), { 0, 1, 4, 1, 4, 5 }); // RIGHT FACE
-	m_indexVer.insert(m_indexVer.end(), { 2, 3, 7, 2, 6, 7 }); // LEFT FACE
-	m_indexVer.insert(m_indexVer.end(), { 0, 3, 4, 3, 4, 7 }); // TOP FACE
-	m_indexVer.insert(m_indexVer.end(), { 1, 2, 5, 2, 5, 6 }); // BTTOM FACE
+	m_indexVer.insert(m_indexVer.end(), { 0, 1, 2, 2, 3, 0 }); // FRONT FACE
+	m_indexVer.insert(m_indexVer.end(), { 1, 5, 6, 6, 2, 1 }); // RIGHT FACE
+	m_indexVer.insert(m_indexVer.end(), { 7, 6, 5, 5, 4, 7 }); // BACK FACE
+	m_indexVer.insert(m_indexVer.end(), { 4, 0, 3, 3, 7, 4 }); // LEFT FACE
+	m_indexVer.insert(m_indexVer.end(), { 4, 5, 1, 1, 0, 4 }); // BOTTOM FACE
+	m_indexVer.insert(m_indexVer.end(), { 3, 2, 6, 6, 7, 3 }); // TOP FACE
 }
--- TEMPLE_RUN/shaders/3D.vs.glsl
+++ TEMPLE_RUN/shaders/3D.vs.glsl
@@ -1,20 +1,18 @@
-#version 330 core
+#version 330
+
+layout(location = 0) in vec3 aVertexPosition; // Position du sommet
+layout(location = 1) in vec3 aVertexNormal; // Normale du sommet
+layout(location = 2) in vec2 aVertexTexCoords; // Coordonnées de texture du sommet
 
 // Matrices de transformations reçues en uniform
 uniform mat4 uMVPMatrix;
 uniform mat4 uMVMatrix;
 uniform mat4 uNormalMatrix;
 
-layout(location = 0) in vec3 aVertexPosition; // Position du sommet
-layout(location = 1) in vec3 aVertexNormal; // Normale du sommet
-layout(location = 2) in vec2 aVertexTexCoords; // Coordonnées de texture du sommet
-
 // Sorties du shader
 out vec3 vPosition_vs; // Position du sommet transformé dans l'espace View
 out vec3 vNormal_vs; // Normale du sommet transformé dans l'espace View
 out vec2 vTexCoords; // Coordonnées de texture du sommet
-
-out vec2 vFragTexture;
 
 void main() {
     // Passage en coordonnées homogènes
@@ -28,5 +26,4 @@ void main() {
 
     // Calcul de la position projetée
     gl_Position = uMVPMatrix * vertexPosition;
-
-};
+}
