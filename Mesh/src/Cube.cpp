d8ccca7c49a1d3e5bc24ceb7b203110938afbe52
ROUSSET CECILE <crouss20@localhost.localdomain>
2018-12-19 17:13:46 +0100

    Normal of the cube fixed + texture

--- Mesh/src/Cube.cpp
+++ Mesh/src/Cube.cpp
@@ -24,31 +24,28 @@ Cube::Cube(const glm::vec3 &width, ShaderManager *shader, Texture *texture) : Me
 
 
 void Cube::generateVertices(const glm::vec3 &width){
-    float w,h,t;
-    w = width.x/2;        
-    h = width.y/2;   
-	t = width.z/2; 
+    // Center the mesh 
+	glm::vec3 upper = glm::vec3(width.x/2., width.y/2., width.z/2.);
+	glm::vec3 lower = glm::vec3(-width.x/2., -width.y/2., -width.z/2.);
 
-	// FRONT FACE
-	//										POSITION			NORMAL				TEXTURE
-    m_vertices.push_back(ShapeVertex(glm::vec3(-w, -h, t), glm::vec3(0, 0, 1), glm::vec2(1,1))); // BOTTOM LEFT
-    m_vertices.push_back(ShapeVertex(glm::vec3(w, -h, t), glm::vec3(0, 0, 1), glm::vec2(1,0))); // BOTTOM RIGHT
-    m_vertices.push_back(ShapeVertex(glm::vec3(w, h, t), glm::vec3(0, 0, 1), glm::vec2(0,0))); // TOP RIGHT
-    m_vertices.push_back(ShapeVertex(glm::vec3(-w, h, t), glm::vec3(0, 0, 1), glm::vec2(0,1))); // TOP LEFT
+	// FRONT FACE										POSITION								NORMAL				TEXTURE
+	m_vertices.push_back(ShapeVertex(glm::vec3(upper.x, upper.y, upper.z), glm::vec3(upper.x, upper.y, upper.z), glm::vec2(1,1))); // TOP RIGHT
+    m_vertices.push_back(ShapeVertex(glm::vec3(upper.x, lower.y, upper.z), glm::vec3(upper.x, lower.y, upper.z), glm::vec2(1,0))); // BOTTOM RIGHT
+    m_vertices.push_back(ShapeVertex(glm::vec3(lower.x, lower.y, upper.z), glm::vec3(lower.x, lower.y, upper.z), glm::vec2(0,0))); // BOTTOM LEFT
+    m_vertices.push_back(ShapeVertex(glm::vec3(lower.x, upper.y, upper.z), glm::vec3(lower.x, upper.y, upper.z), glm::vec2(0,1))); // TOP LEFT
 
-    // BACK FACE
-	//										POSITION			NORMAL				TEXTURE
-    m_vertices.push_back(ShapeVertex(glm::vec3(-w, -h, -t), glm::vec3(0, 0, -1), glm::vec2(1,1))); // TOP RIGHT
-    m_vertices.push_back(ShapeVertex(glm::vec3(w, -h, -t), glm::vec3(0, 0, -1), glm::vec2(1,0))); // BOTTOM RIGHT
-    m_vertices.push_back(ShapeVertex(glm::vec3(w, h, -t), glm::vec3(0, 0, -1), glm::vec2(0,0))); // BOTTOM LEFT
-    m_vertices.push_back(ShapeVertex(glm::vec3(-w, h, -t), glm::vec3(0, 0, -1), glm::vec2(0,1))); // TOP LEFT
+	// BACK FACE										POSITION								NORMAL				TEXTURE
+	m_vertices.push_back(ShapeVertex(glm::vec3(upper.x, upper.y, lower.z), glm::vec3(upper.x, upper.y, lower.z), glm::vec2(0,1))); // TOP RIGHT
+	m_vertices.push_back(ShapeVertex(glm::vec3(upper.x, lower.y, lower.z), glm::vec3(upper.x, lower.y, lower.z), glm::vec2(0,0))); // BOTTOM RIGHT
+	m_vertices.push_back(ShapeVertex(glm::vec3(lower.x, lower.y, lower.z), glm::vec3(lower.x, lower.y, lower.z), glm::vec2(1,0))); // BOTTOM LEFT
+	m_vertices.push_back(ShapeVertex(glm::vec3(lower.x, upper.y, lower.z), glm::vec3(lower.x, upper.y, lower.z), glm::vec2(1,1))); // TOP LEFT
 }
 
 void Cube::generateIndexs(){
-	m_indexVer.insert(m_indexVer.end(), { 0, 1, 2, 2, 3, 0 }); // FRONT FACE
-	m_indexVer.insert(m_indexVer.end(), { 1, 5, 6, 6, 2, 1 }); // RIGHT FACE
-	m_indexVer.insert(m_indexVer.end(), { 7, 6, 5, 5, 4, 7 }); // BACK FACE
-	m_indexVer.insert(m_indexVer.end(), { 4, 0, 3, 3, 7, 4 }); // LEFT FACE
-	m_indexVer.insert(m_indexVer.end(), { 4, 5, 1, 1, 0, 4 }); // BOTTOM FACE
-	m_indexVer.insert(m_indexVer.end(), { 3, 2, 6, 6, 7, 3 }); // TOP FACE
+	m_indexVer.insert(m_indexVer.end(), { 0, 1, 2, 0, 2, 3 }); // FRONT FACE
+	m_indexVer.insert(m_indexVer.end(), { 4, 5, 6, 4, 6, 7 }); // BACK FACE
+	m_indexVer.insert(m_indexVer.end(), { 0, 1, 4, 1, 4, 5 }); // RIGHT FACE
+	m_indexVer.insert(m_indexVer.end(), { 2, 3, 7, 2, 6, 7 }); // LEFT FACE
+	m_indexVer.insert(m_indexVer.end(), { 0, 3, 4, 3, 4, 7 }); // TOP FACE
+	m_indexVer.insert(m_indexVer.end(), { 1, 2, 5, 2, 5, 6 }); // BOTTOM FACE
 }
