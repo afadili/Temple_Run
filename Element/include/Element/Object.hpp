30f5f6f8c0393b63dc166203935926dcb208cf42
Nicolas Sénécal <nicolassenecal83@gmail.com>
2018-12-20 12:06:43 +0100

    Added Object class

---
+++ Element/include/Element/Object.hpp
@@ -0,0 +1,100 @@
+/**
+ * \file Object.hpp
+ * \brief Declaration of the class "Object"
+ */
+
+#ifndef __OBJECT__HPP
+#define __OBJECT__HPP
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <iostream>
+
+#include <Mesh/Mesh.hpp>
+
+/**
+ * \class Object
+ * \brief Mother class of the objects on the map
+ */
+class Object {
+protected:
+  Mesh *m_mesh; /*!< Pointer on the mesh corresponding to the object */
+  glm::vec3 m_position = glm::vec3(0, 0, 0); /*!< Position of the object in the world coordinates */
+  glm::vec3 m_size = glm::vec3(1, 1, 1); /*!< Size of the object in the world coordinates */
+  glm::vec3 m_rotation = glm::vec3(0, 0, 0); /*!< Rotation of the object in the world coordinates */
+
+private:
+  Object();
+
+public:
+
+  /**
+   * \brief Constructor with mesh
+   * \param mesh : pointer on the mesh corresponding to the object
+   */
+  Object(Mesh *mesh);
+
+
+  /**
+   * \brief Constructor with mesh and parameters
+   * \param mesh : pointer on the mesh corresponding to the object
+   */
+  Object(Mesh *mesh, const glm::vec3 &position);
+
+  /**
+   * \brief Constructor with mesh and parameters
+   * \param mesh : pointer on the mesh corresponding to the object
+   * \param position : position of the object in the world coordinates
+   * \param size : size of the object in the world coordinates
+   */
+  Object(Mesh *mesh, const glm::vec3 &position, const glm::vec3 &size);
+
+  /**
+   * \brief Constructor with mesh and parameters
+   * \param mesh : pointer on the mesh corresponding to the object
+   * \param position : position of the object in the world coordinates
+   * \param size : size of the object in the world coordinates
+   * \param rotation : rotation of the object in the world coordinates
+   */
+  Object(Mesh *mesh, const glm::vec3 &position, const glm::vec3 &size, const glm::vec3 &rotation);
+
+  /**
+   * \brief Translate the object
+   * \param vec : translation vector
+   * \return the new world coordinates matrix of the object
+   */
+  glm::mat4 translate(const glm::vec3 &vec);
+
+  /**
+   * \brief Rotate the object (in radian)
+   * \param vec : rotation vector
+   * \return the new world coordinates matrix of the object
+   */
+  glm::mat4 rotate(const glm::vec3 &vec);
+
+  /**
+   * \brief Scale the object
+   * \param vec : scale vector
+   * \return the new world coordinates matrix of the object
+   */
+  glm::mat4 scale(const glm::vec3 &vec);
+
+  /**
+   * \brief Get the world coordinates matrix of the object 
+   * \return world coordinates matrix of the object 
+   */
+  glm::mat4 MWMatrix() const;
+
+  /**
+   * \brief Getter of mesh
+   * \return m_mesh
+   */
+  inline
+  Mesh *mesh() const {
+    return m_mesh;
+  }
+
+
+};
+
+#endif
---
+++ Element/src/Object.cpp
@@ -0,0 +1,40 @@
+#include <Element/Object.hpp>
+
+Object::Object(Mesh *mesh) : m_mesh(mesh) {
+}
+
+Object::Object(Mesh *mesh, const glm::vec3 &position) :
+m_mesh(mesh), m_position(position) {
+}
+
+Object::Object(Mesh *mesh, const glm::vec3 &position, const glm::vec3 &size) :
+m_mesh(mesh), m_position(position), m_size(size) {
+}
+
+Object::Object(Mesh *mesh, const glm::vec3 &position, const glm::vec3 &size, const glm::vec3 &rotation) :
+m_mesh(mesh), m_position(position), m_size(size), m_rotation(rotation) {
+}
+
+glm::mat4 Object::translate(const glm::vec3 &vec) {
+  m_position += vec;
+  return MWMatrix();
+}
+
+glm::mat4 Object::rotate(const glm::vec3 &vec) {
+  m_rotation += vec;
+  return MWMatrix();
+}
+
+glm::mat4 Object::scale(const glm::vec3 &vec) {
+  m_size += vec;
+  return MWMatrix();
+}
+
+glm::mat4 Object::MWMatrix() const {
+  glm::mat4 MWMatrix = glm::translate(glm::mat4(), m_position);
+  MWMatrix = glm::rotate(MWMatrix, m_rotation.x, glm::vec3(1.f, 0, 0));
+  MWMatrix = glm::rotate(MWMatrix, m_rotation.y, glm::vec3(0, 1.f, 0));
+  MWMatrix = glm::rotate(MWMatrix, m_rotation.z, glm::vec3(0, 0, 1.f));
+  MWMatrix = glm::scale(MWMatrix, m_size);
+  return MWMatrix;
+}
