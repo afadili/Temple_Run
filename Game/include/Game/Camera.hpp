1060b1226d782f13bd9ce465aa20fedeecf4217b
cecilerousset <rousset.cecile13@orange.fr>
2019-01-04 17:37:22 +0100

    New Camera

--- Game/include/Game/Camera.hpp
+++ Game/include/Game/Camera.hpp
@@ -20,17 +20,31 @@ class Camera {
 private:
 
   glm::vec3 m_center; /*!< position of the center of the camera */
-  float m_fDistance; /*!< position of the center of the camera */
+
+  float m_fDistance; /*!< distance from the center of the scene */
   float m_fAngleX; /*!< Angle around the X-axis of the camera */
   float m_fAngleY; /*!< Angle around the Y-axis of the camera */
-  float m_fPhi; /*!< Angle around the Y-axis of the camera */
-  float m_fTheta; /*!< Angle around the Y-axis of the camera */
+
+  float m_fPhi; /*!< spherical coordinates of the the vector F */
+  float m_fTheta; /*!< sperical coordinates of the vector F */
   glm::vec3 m_frontVector; /*!< Vector F */
   glm::vec3 m_leftVector; /*!< Vector L */
   glm::vec3 m_upVector; /*!< Vector U */
-  bool m_isFPS = false; /*!< if the camera is in fps mode */
+
+  bool KEY_UP_PRESSED = false; /*!< keyboard action */
+  bool KEY_DOWN_PRESSED = false; /*!< keyboard action*/
+  bool KEY_LEFT_PRESSED = false; /*!< keyboard action*/
+  bool KEY_RIGHT_PRESSED = false; /*!< keyboard action*/
+
+  unsigned int m_currentCamera; /*!< 1 = third person */
+                                /*!< 2 = first person */
+                                /*!< 3 = rotation */
+                                /*!< 4 = freefly camera */
+
 
 public:
+  static const float VIEW_WIDTH;
+
   /**
    * \brief constructor of Camera
    */
@@ -43,41 +57,76 @@ public:
   }
 
   /**
-   * \brief method to move the camera with the character's view
+   * \brief modification of the camera depending on the events
+   * \param e : SDL event
    */
-  void moveUp();
+  void eventManager(const SDL_Event &e);
 
   /**
-   * \brief method to move the camera with the character's view
+   * \brief update the position of the camera
    */
-  void moveForward(const float &x);
+  void update(const glm::vec3 &center);
 
   /**
-   * \brief method to move the camera with the character's view
+   * \brief 
+   * \param scale
+   * \param angles
    */
-  void moveLeft(const float &x);
+  void characterIntel(const float &scale, const glm::vec3 &angles);
 
   /**
-   * \brief method to move the camera with the character's view
-   */
-  void moveRight(const float &y);
-
-  /**
-   * \brief method that update the position of the camera
-   */
-  void Update() {
-  }
-
-  /**
-   * \brief setter that reads the event and will set the camera depending on the event
-   */
-  void readEvent(const SDL_Event &e);
-
-  /**
-   * \brief getter of the camera's viewMatrix
+   * \brief get view matrix
    */
   glm::mat4 getViewMatrix() const;
 
+private:
+
+  /**
+   * \brief to move forward in third person
+   * \param delta
+   */
+  void moveForwardThirdPerson(const float &delta);
+
+  /**
+   * \brief to rotate left in third person
+   * \param degrees
+   */
+  void rotateLeftThirdPosition(const float &degrees);
+
+  /**
+   * \brief to rotate right in third person
+   * \param degrees
+   */
+  void rotateRightThirdPosition(const float &degrees);
+
+  /**
+   * \brief to move forward in first person
+   * \param t
+   */
+  void moveForwardFirstPerson(const float &t);
+
+  /**
+   * \brief to move left in first person
+   * \param t
+   */
+  void moveLeftFirstPerson(const float &t);
+
+  /**
+   * \brief to rotate in first person
+   * \param t
+   */
+  void rotateFrontFirstPerson(const float &degrees);
+
+  /**
+   * \brief to rotate left in first person
+   * \param degrees
+   */
+  void rotateLeftFirstPerson(const float &degrees);
+
+  /**
+   * \brief direction vector calculator
+   */
+  void computeDirectionVectors();
 };
 
 #endif
--- Game/src/Camera.cpp
+++ Game/src/Camera.cpp
@@ -1,66 +1,237 @@
 #include <Game/Camera.hpp>
 
-Camera::Camera() : m_fDistance(-15.0f), m_fAngleX(0.0f), m_fAngleY(0.0f), m_center(0.f, 0.f, 0.f) {
+const double PI = 3.141592653589;
+const double HALF_PI = PI / 2;
+const float Camera::VIEW_WIDTH = 30.f;
+
+Camera::Camera() : m_fDistance(-15.0f), m_fAngleX(0.0f), m_fAngleY(0.0f), m_center(0.f, 0.f, 0.f), m_currentCamera(1) {
   computeDirectionVectors();
 };
 
-void Camera::moveUp() {
-}
+void Camera::event(const SDL_Event &e){
 
-void Camera::moveForward(const float &x) {
-  if (x > 0 && m_center >= -1.0f)
-    return;
-  m_center += x;
-}
-
-void Camera::moveLeft(const float &x) {
-  m_rotateX += x;
-}
-
-void Camera::moveRight(const float &y) {
-  m_rotateY += y;
-}
-
-void Camera::readEvent(const SDL_Event &e) {
-  switch (e.type) {
-      // Touche du clavier
-    case SDL_KEYDOWN:
-    {
-      float step = 1.0f;
-      if (e.key.keysym.sym == SDLK_z || e.key.keysym.sym == SDLK_UP) // touche de Haut
-      {
-        moveForward(step);
-      } else if (e.key.keysym.sym == SDLK_s || e.key.keysym.sym == SDLK_DOWN) // touche de bas 
-      {
-        moveForward(-step);
+  // Change the point of view
+  if (e.type == SDL_KEYDOWN){
+    if (e.key.keysym.sym == SDLK_c){
+      if (m_currentCamera != 1)
+        m_currentCamera = 1;
+      else{
+        m_currentCamera = 2;
+        m_fTheta = glm::radians(m_fAngleX);
+        m_fPhi = glm::radians(-m_fAngleY);
+        computeDirectionVectors();
       }
     }
-      break;
-      // la souris 
+    if (e.key.keysym.sym == SDLK_v && DEBUG)
+      m_currentCamera = 3;
+    if (e.key.keysym.sym == SDLK_b && DEBUG)
+      m_currentCamera = 4;
+    std::cout << "Changement de camera : " << m_currentCamera << std::endl;
+  }
+
+  // Handle the keys for the rotating camera
+  if (m_currentCamera == 3){
+    switch (e.type){
+    /* Touche clavier */
+    case SDL_KEYDOWN:
+    {
+      float zoom = 1.0f;
+      if (e.key.keysym.sym == SDLK_z || e.key.keysym.sym == SDLK_UP){
+        //std::cout << "Z or UP pressed" << std::endl;
+        TPMoveFront(zoom);
+      }
+      else if (e.key.keysym.sym == SDLK_s || e.key.keysym.sym == SDLK_DOWN){
+        //std::cout << "S or DOWN pressed" << std::endl;
+        TPMoveFront(-zoom);
+      }
+    }
+    break;
+
     case SDL_MOUSEMOTION:
     {
       float speed = 1.f;
-      if (e.motion.xrel != 0) {
-        moveRight(float(e.motion.xrel) * speed);
+      if (e.motion.xrel != 0){
+        TPRotateRight(float(e.motion.xrel) * speed);
       }
-      if (e.motion.yrel != 0) {
-        moveLeft(float(e.motion.yrel) * speed);
+      if (e.motion.yrel != 0){
+        TPRotateLeft(float(e.motion.yrel) * speed);
       }
-
     }
-      break;
+    break;
 
     default:
       break;
+    }
+  }
+
+  //Handle the keys for the first person
+  else if (m_currentCamera == 2 || m_currentCamera == 4){
+    switch (e.type){
+    /* Touche clavier DOWN */
+    case SDL_KEYDOWN:
+    {
+      if (m_currentCamera == 4){
+        if (e.key.keysym.sym == SDLK_z || e.key.keysym.sym == SDLK_UP){
+          KEY_UP_PRESSED = true;
+        }
+        if (e.key.keysym.sym == SDLK_s || e.key.keysym.sym == SDLK_DOWN){
+          KEY_DOWN_PRESSED = true;
+        }
+        if (e.key.keysym.sym == SDLK_q || e.key.keysym.sym == SDLK_LEFT){
+          KEY_LEFT_PRESSED = true;
+        }
+        if (e.key.keysym.sym == SDLK_d || e.key.keysym.sym == SDLK_RIGHT){
+          KEY_RIGHT_PRESSED = true;
+        }
+      }
+    }
+    break;
+
+    case SDL_KEYUP:
+    {
+      if (m_currentCamera == 4){
+        if (e.key.keysym.sym == SDLK_z || e.key.keysym.sym == SDLK_UP){
+          KEY_UP_PRESSED = false;
+        }
+        if (e.key.keysym.sym == SDLK_s || e.key.keysym.sym == SDLK_DOWN){
+          KEY_DOWN_PRESSED = false;
+        }
+        if (e.key.keysym.sym == SDLK_q || e.key.keysym.sym == SDLK_LEFT){
+          KEY_LEFT_PRESSED = false;
+        }
+        if (e.key.keysym.sym == SDLK_d || e.key.keysym.sym == SDLK_RIGHT){
+          KEY_RIGHT_PRESSED = false;
+        }
+      }
+    }
+    break;
+
+    case SDL_MOUSEMOTION:
+    {
+      float speed = 0.5f;
+      std::cout << "Mouse move: ";
+      std::cout << e.motion.xrel << " | " << e.motion.yrel << " | Phi: " << m_fPhi << " | Theta: " << m_fTheta << std::endl;
+      if (e.motion.xrel != 0)
+      {
+        FPRotateFront(float(-e.motion.xrel) * speed);
+      }
+      if (e.motion.yrel != 0)
+      {
+        FPRotateLeft(float(e.motion.yrel) * speed);
+      }
+    }
+    break;
+
+    default:
+      break;
+    }
   }
 }
 
-glm::mat4 Camera::getViewMatrix() const {
-  glm::mat4 VM = glm::mat4(1.f);
-  VM = glm::translate(VM, glm::vec3(0.0f, 0.0f, m_center));
-  VM = glm::rotate(VM, glm::radians(m_rotateX), glm::vec3(1.0f, 0.0f, 0.0f));
-  VM = glm::rotate(VM, glm::radians(m_rotateY), glm::vec3(0.0f, 1.0f, 0.0f));
-  VM = glm::translate(VM, -m_position);
-  return VM;
 
+void Camera::update(const glm::vec3 &center){
+  /* CONTROL */
+  if (m_currentCamera == 4){
+    float speed = 0.1f;
+    if (KEY_UP_PRESSED){
+      FPMoveFront(speed);
+    }
+    else if (KEY_DOWN_PRESSED){
+      FPMoveFront(-speed);
+    }
+    else if (KEY_LEFT_PRESSED){
+      KEY_LEFT_PRESSED = true;
+      FPMoveLeft(speed);
+    }
+    else if (KEY_RIGHT_PRESSED){
+      KEY_RIGHT_PRESSED = true;
+      FPMoveLeft(-speed);
+    }
+  }
+  else if (m_currentCamera == 2){
+    m_center = center + glm::vec3(0.f, 1.f, 0.f);
+  }
+  else {
+    m_center = center;
+  }
+}
+
+void Camera::characterIntel(const float &scale, const glm::vec3 &angles){
+  m_fDistance = scale * -10.0f;
+  m_fAngleX = 10.f;
+  m_fAngleY = -angles[Y];
+  m_fPhi = glm::radians(m_fAngleY);
+  m_fTheta = 0.f;
+  computeDirectionVectors();
+}
+
+glm::mat4 Camera::getViewMatrix() const{
+  if (m_currentCamera == 2 || m_currentCamera == 4){
+    return glm::lookAt(m_center, m_center + m_frontVector, m_upVector);
+  }
+  else{
+    glm::mat4 VM = glm::mat4(1.f);
+    VM = glm::translate(VM, glm::vec3(0.0f, 0.0f, m_fDistance));
+    VM = glm::rotate(VM, glm::radians(m_fAngleX), glm::vec3(1.0f, 0.0f, 0.0f));
+    VM = glm::rotate(VM, glm::radians(m_fAngleY + 180.f), glm::vec3(0.0f, 1.0f, 0.0f));
+    VM = glm::translate(VM, -m_center);
+    if (m_currentCamera == 1){
+      VM = glm::translate(VM, glm::vec3(0.f, -1.f, 0.f));
+    }
+    return VM;
+  }
+}
+
+// ========================== THIRD PERSON ========================
+void Camera::moveForwardThirdPerson(const float &delta){
+  if (delta > 0 && m_fDistance >= -1.0f)
+    return;
+  m_fDistance += delta;
+}
+
+void Camera::rotateLeftThirdPerson(const float &degrees){
+  m_fAngleX += degrees;
+}
+
+void Camera::rotateRightThirdPosition(const float &degrees){
+  m_fAngleY += degrees;
+}
+
+// ========================== FIRST PERSON ========================
+void Camera::moveForwardFirstPerson(const float &t){
+  m_center += t * m_frontVector;
+  m_upVector = glm::cross(m_frontVector, m_leftVector);
+}
+
+void Camera::moveLeftFirstPerson(const float &t){
+  m_center += t * m_leftVector;
+  m_upVector = glm::cross(m_frontVector, m_leftVector);
+}
+
+void Camera::rotateFrontFirstPerson(const float &degrees){
+  m_fPhi += glm::radians(degrees);
+  if (m_currentCamera == 2){
+    m_fPhi = Utils::clamp(m_fPhi, glm::radians(-m_fAngleY - Camera::VIEW_WIDTH), glm::radians(-m_fAngleY + Camera::VIEW_WIDTH));
+  }
+  m_frontVector = glm::vec3(std::cos(m_fTheta) * std::sin(m_fPhi), std::sin(m_fTheta), std::cos(m_fTheta) * std::cos(m_fPhi));
+
+  m_leftVector = glm::vec3(std::sin(m_fPhi + HALF_PI), 0, std::cos(m_fPhi + HALF_PI));
+}
+
+void Camera::rotateLeftFirstPerson(const float &degrees){
+  m_fTheta += glm::radians(degrees);
+  if (m_currentCamera == 2){
+    m_fTheta = Utils::clamp(m_fTheta, glm::radians(-Camera::VIEW_WIDTH), glm::radians(Camera::VIEW_WIDTH));
+  }
+  m_frontVector = glm::vec3(std::cos(m_fTheta) * std::sin(m_fPhi), std::sin(m_fTheta), std::cos(m_fTheta) * std::cos(m_fPhi));
+
+  m_leftVector = glm::vec3(std::sin(m_fPhi + HALF_PI), 0, std::cos(m_fPhi + HALF_PI));
+}
+
+void Camera::computeDirectionVectors(){
+  m_frontVector = glm::vec3(std::cos(m_fTheta) * std::sin(m_fPhi), std::sin(m_fTheta), std::cos(m_fTheta) * std::cos(m_fPhi));
+
+  m_leftVector = glm::vec3(std::sin(m_fPhi + HALF_PI), 0, std::cos(m_fPhi + HALF_PI));
+
+  m_upVector = glm::cross(m_frontVector, m_leftVector);
 }
